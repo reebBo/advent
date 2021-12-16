3cd1299e024a1b55dedce0e8b0a1058302a4a343
rebecaLH <rebeca@levvel.health>
2021-12-16 22:07:04 +0100

    remove alert on today; gighlight the day with ngClass

--- src/app/components/calendar/calendar.component.ts
+++ src/app/components/calendar/calendar.component.ts
@@ -8,18 +8,16 @@ import { SharedService } from 'src/app/services/shared.service';
   styleUrls: ['./calendar.component.scss'],
 })
 export class CalendarComponent implements OnInit {
+  // active: boolean = false;
   constructor(private router: Router, private sharedS: SharedService) {}
-
 
   date = new Date();
   day = this.date.getDate();
 
-
   ngOnInit(): void {
     // let da = new Intl.DateTimeFormat('en', { day: '2-digit' }).format(today);
-    alert('Astăzi este ziua '+ this.day + ' de Advent!'); 
+    // alert('Astăzi este ziua ' + this.day + ' de Advent!');
   }
-
 
   //make days clickable only on specific days, based on date.
   goToContent(id: any) {
@@ -27,14 +25,13 @@ export class CalendarComponent implements OnInit {
     if (this.day == 30) {
       selectedDay = 1;
     } else {
-      selectedDay = this.day;  
+      selectedDay = this.day; 
     }
     // display today's day and previous
     if (id <= selectedDay) {
       // make id available in day component to filter content based on it
       this.sharedS.updateId(id);
       this.router.navigateByUrl('/day');
-      
     } else {
       alert(
         '😁 Aștepți cu nerăbdare să vezi ce urmează, nu-i așa? 😁 Am pregătit ceva drăguț pentru zilele următoare, te asigur! 😁'
@@ -43,29 +40,29 @@ export class CalendarComponent implements OnInit {
   }
 
   days = [
-    { id: 1},
-    { id: 2},
-    { id: 3},
-    { id: 4},
-    { id: 5},
-    { id: 6},
-    { id: 7},
-    { id: 8},
-    { id: 9},
-    { id: 10},
-    { id: 11},
-    { id: 12},
-    { id: 13},
-    { id: 14},
-    { id: 15},
-    { id: 16},
-    { id: 17},
-    { id: 18},
-    { id: 19},
-    { id: 20},
-    { id: 21},
-    { id: 22},
-    { id: 23},
-    { id: 24},
+    { id: 1 },
+    { id: 2 },
+    { id: 3 },
+    { id: 4 },
+    { id: 5 },
+    { id: 6 },
+    { id: 7 },
+    { id: 8 },
+    { id: 9 },
+    { id: 10 },
+    { id: 11 },
+    { id: 12 },
+    { id: 13 },
+    { id: 14 },
+    { id: 15 },
+    { id: 16 },
+    { id: 17 },
+    { id: 18 },
+    { id: 19 },
+    { id: 20 },
+    { id: 21 },
+    { id: 22 },
+    { id: 23 },
+    { id: 24 },
   ];
 }
