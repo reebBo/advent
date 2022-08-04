7abbdabba33ac228ce9a10c483a1edbdd749dd52
rebecaLH <76942384+rebecaLH@users.noreply.github.com>
2022-08-04 20:26:20 +0200

    Create daily-content.service.ts
    remove http calls from component to daily-content.service
    getCraft() implemented in daily-content.service

---
+++ src/services/daily-content.service.ts
@@ -0,0 +1,16 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DailyContentService {
+
+  constructor( private http: HttpClient) { }
+
+
+  getCraft():Observable<Object> {
+   return this.http.get('../../../assets/jsonContent/craft.json');
+  }
+}
