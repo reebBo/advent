d387881b56e141208f959fbc3559b855df26f403
rebecaLH <rebeca@levvel.health>
2022-08-16 15:47:16 +0200

    Get the type of browser and its version

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,53 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent { 
+export class AppComponent {
+  getExplorerInfo() {
+    let t:any = navigator.userAgent.toLowerCase();
+    alert(JSON.stringify(t))
 
+    return 0 <= t.indexOf('msie')
+      ? {
+          //ie < 11
+          type: 'IE',
+          version: Number(t.match(/msie ([\d]+)/)[1]),
+        }
+      : !!t.match(/trident\/.+?rv:(([\d.]+))/)
+      ? {
+          // ie 11
+          type: 'IE',
+          version: 11,
+        }
+      : 0 <= t.indexOf('edge')
+      ? {
+          type: 'Edge',
+          version: Number(t.match(/edge\/([\d]+)/)[1]),
+        }
+      : 0 <= t.indexOf('firefox')
+      ? {
+          type: 'Firefox',
+          version: Number(t.match(/firefox\/([\d]+)/)[1]),
+        }
+      : 0 <= t.indexOf('chrome')
+      ? {
+          type: 'Chrome',
+          version: Number(t.match(/chrome\/([\d]+)/)[1]),
+        }
+      : 0 <= t.indexOf('opera')
+      ? {
+          type: 'Opera',
+          version: Number(t.match(/opera.([\d]+)/)[1]),
+        }
+      : 0 <= t.indexOf('Safari')
+      ? {
+          type: 'Safari',
+          version: Number(t.match(/version\/([\d]+)/)[1]),
+        }
+      : {
+          type: t,
+          version: -1,
+        };
+  };
 }
