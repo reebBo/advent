168473cb6098170ec1f9efbc8075b0b27ef87d3e
rebecaLH <rebeca@levvel.health>
2023-01-19 20:12:12 +0100

    cleanup reactive form code in footer

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,6 +1,4 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
-import { ModalDirective } from 'ngx-bootstrap/modal';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
@@ -8,12 +6,6 @@ import { ModalDirective } from 'ngx-bootstrap/modal';
   styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent implements OnInit {
-  @ViewChild('autoShownModal', { static: false }) autoShownModal?: ModalDirective;
-  isModalShown = false;
-  // -----------
-  form!: FormGroup;
-  submitted = false;
-  // -----------
   today = new Date();
   decemberFirst = new Date(this.today.getFullYear(), 11, 1);
   millisecondsDay = 1000 * 60 * 60 * 24;
@@ -23,20 +15,9 @@ export class FooterComponent implements OnInit {
   day = this.today.getDate();
   month = this.today.getMonth() + 1;
 
-  // -----------  
-  constructor(private formBuilder: FormBuilder) { }
-  // formValue = localStorage.getItem('form-data');
-
+  constructor() { }
 
   ngOnInit(): void {
-    // localStorage.clear();
-
-    // this.form = this.formBuilder.group(
-    //   {
-    //     full_name: ['', Validators.required],
-    //     email: ['', [Validators.required, Validators.email]],
-    //     acceptTerms: [false, Validators.requiredTrue]
-    //   });
     let difference = +this.decemberFirst - +this.today;
     this.noOfDaysToAdvent = Math.ceil(difference / this.millisecondsDay);
   }
