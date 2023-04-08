2cb76bf90e2563a928e446718adc509ee5213e1d
rebecaLH <rebeca@levvel.health>
2023-04-09 00:07:48 +0200

    comm

--- src/app/components/contest/contest.component.spec.ts
+++ src/app/components/contest/contest.component.spec.ts
@@ -1,7 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ContestComponent } from './contest.component';
-//need tests
+//need to add tests 
 describe('ContestComponent', () => {
   let component: ContestComponent;
   let fixture: ComponentFixture<ContestComponent>;
